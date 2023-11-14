63b7b26a811feb620d9cf6c2433bed2bd0878237
NoTutou <111877658+NoTutou@users.noreply.github.com>
2023-11-14 03:39:33 +0000

    first commit

---
+++ demo/src/main/java/com/test/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.test.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+        int n = 5; // 可以改变这个值来打印不同大小的三角形
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
